4e02ee2c36edd37e95a01dc20d9633373435dc93
youtao1999 <ty296@physics.rutgers.edu>
2025-10-11 19:32:29 -0400

    first working version of the passthrough adder.

--- benchmark_adder.jl
+++ benchmark_adder.jl
@@ -1,129 +1,39 @@
 using Pkg
 Pkg.activate("CT")
 using CT
-include("global_adder_passthrough.jl")
-
+# benchmark adder_MPO
+using ITensors
+# initialize random state
+using .CT: CT_MPS
+using Random
 using ProgressMeter
-# benchmark adder_MPO
-using ITensors
-# initialize random state
-using .CT: _initialize_basis, _initialize_vector, P_MPO, XI_MPO, I_MPO, adder_MPO, add1, power_mpo
-using Random
 
-using Pkg
-Pkg.activate("CT")
-using CT
-
-# benchmark adder_MPO
-using ITensors
-# initialize random state
-using .CT: _initialize_basis, _initialize_vector, P_MPO, XI_MPO, I_MPO, adder_MPO, add1, power_mpo
-using Random
-
-L = 8
+L = 24
 ancilla = 0
 folded = true
 seed_vec = 123457
 xj = Set([1//3, 2//3])
-i1 = 1
-numerator = 1
-denominator = 3
 _maxdim = 2^(div(L,2))
-_maxdim0 = 2^10
-_eps = 1e-10
+_maxdim0 = 50
+_eps = 0.0
 seed = 123457
 x0 = nothing
-qubit_site, ram_phy, phy_ram, phy_list = _initialize_basis(L, ancilla, folded)
 rng = MersenneTwister(seed_vec)
 rng_vec = seed_vec === nothing ? rng : MersenneTwister(seed_vec)
-shift_1_3_bits, shift_1_3_amount = fraction_to_binary_shift(numerator, denominator, L)
-initial_state = _initialize_vector(L, ancilla, x0, folded, qubit_site, ram_phy, phy_ram, phy_list, rng_vec, _eps, _maxdim0);
+ct_h = CT_MPS(L=L,xj=xj,folded=folded,_maxdim=_maxdim,ancilla=ancilla,seed_vec=seed_vec,seed=seed,x0=x0,debug=false);
+ct_t = CT_MPS(L=L,xj=xj,folded=folded,_maxdim=_maxdim,ancilla=ancilla,seed_vec=seed_vec,seed=seed,x0=x0,debug=false,passthrough=true);
 
-# initialize haining adder mpo
-add1_mpo=MPO(add1(i1,L,phy_ram,phy_list),qubit_site)
-add1_6,add1_3=power_mpo(add1_mpo,[div(2^L,6)+1,div(2^L,3)])
+global i, j
+i = 1
+j = 1
+p_ctrl = 0.4
+p_proj = 0.7
 
-function display_state(state)
-    contracted = contract(state)
-    state_vec = vec(Array(contracted, inds(contracted)...))
-    # @show argmax(state_vec)
-    loc = findall(x->abs(x)>1e-1, state_vec)
-    # println(state_vec[loc])
-    if length(loc) > 0
-        pos = loc[1] - 1
-        # ITensors native ordering: site 1 = rightmost bit in binary position
-        # We reversed input, so reverse output to match our convention
-        binary = string(pos, base=2, pad=L)
-        return reverse(binary)  # site 1 = leftmost bit in our convention
-    else
-        return "0"^L
-    end
-end
-# create_addition_tensor_with_carry(shift_bit::Int, s::Index, s_prime::Index, c_in::Index, c_out::Index)
-carry_links, T_vec, id_vec, gate_vec = initialize_links(L, qubit_site, shift_1_3_bits, ram_phy);
-adder_passthrough = build_adder_mpo(qubit_site,L,carry_links,gate_vec,_eps,_maxdim);
-
-# prod(gate_vec)
-
-dict_correct = Dict{String, String}()
-
-if abspath(PROGRAM_FILE) == @__FILE__
-
-    for state_index in 1:2^L
-        string_vec = lpad(string(state_index-1,base=2),L,"0")
-        correct_unfolded = lpad(string((state_index - 1 + shift_1_3_amount) % 2^L,base=2),L,"0")
-        string_vec_folded = join([string_vec[ram_phy[i]] for i in 1:L])
-        correct_folded = join([correct_unfolded[ram_phy[i]] for i in 1:L])
-        dict_correct[join(string_vec)] = join(correct_folded)
-    end
-
-
-    @showprogress for state_index in 1:2^L
-        string_vec = lpad(string(state_index-1,base=2),L,"0")
-        string_vec_folded = join([string_vec[ram_phy[i]] for i in 1:L])
-        # Convention: RAM position i stores physical bit ram_phy[i]
-        # productMPS assigns to RAM positions, so use folded representation
-        vec = 1 .+ parse.(Int, [string(string_vec_folded[i]) for i in 1:L])
-        initial_state = productMPS(qubit_site, vec)
-
-        # @show CT.mps_element(initial_state, "000000001000"[ram_phy])
-        final_state_1 = copy(initial_state)
-        final_state_2 = copy(initial_state)
-        # passthrough_state = copy(initial_state)
-        final_state_1 = apply(adder_passthrough,final_state_1; cutoff=_eps, maxdim=_maxdim);
-        # passthrough_state = global_adder(passthrough_state, carry_links, T_vec, gate_vec, qubit_site; cutoff=_eps, maxdim=_maxdim);
-        final_state_2 = apply(add1_3,final_state_2; cutoff=_eps, maxdim=_maxdim);
-        # @show CT.mps_element(final_state_2, "001010110011"[ram_phy])
-        # @show display_state(final_state_2)[phy_ram]
-        final_state_1_unfolded = [ display_state(final_state_1)[phy_ram[i]] for i in 1:L ]
-        final_state_2_unfolded = [ display_state(final_state_2)[phy_ram[i]] for i in 1:L ]
-        # passthrough_state_unfolded = [ display_state(passthrough_state)[phy_ram[i]] for i in 1:L ]
-        # println(string_vec, "=>", join(final_state_1_unfolded))
-        # println(string_vec, "=>", join(final_state_2_unfolded))
-        if join(final_state_1_unfolded) != join(final_state_2_unfolded)
-            println(string_vec)
-            println(join(final_state_1_unfolded))
-            println(join(final_state_2_unfolded))
-            # println(dict_correct[join(string_vec)])
-            # println(join(passthrough_state_unfolded))
-            println("wrong")
-            # println("correct: ", dict_correct[join(string_vec)])
-
-        end
-
-        # println(string_vec, "=>", display_state(final_state_1), "<=>", display_state(final_state_2))
-        # println(display_state(final_state_1) == display_state(final_state_2))
-    end
-
-    # # warm up
-    # initial_state = randomMPS(qubit_site, L)
-    # final_state_1 = copy(initial_state)
-    # final_state_2 = copy(initial_state)
-    # final_state_1 = global_adder(final_state_1, carry_links, T_vec, gate_vec, qubit_site; cutoff=_eps, maxdim=_maxdim);
-    # final_state_2 = apply(add1_6,final_state_2; cutoff=_eps, maxdim=_maxdim);
-
-    # # benchmark
-    # @time final_state_2 = apply(add1_6,final_state_2; cutoff=_eps, maxdim=_maxdim);
-
-    # @time final_state_1 = global_adder(final_state_1, carry_links, T_vec, gate_vec, qubit_site; cutoff=_eps, maxdim=_maxdim);
+ancilla = 0
+T_max = ancilla == 0 ? 2*(ct_h.L^2) : div(ct_h.L^2,2)
+# Write a single string
+for _ in 1:T_max
+    global i, j
+    @time i = CT.random_control!(ct_h,i,p_ctrl,p_proj)
+    @time j = CT.random_control!(ct_t,j,p_ctrl,p_proj)
 end
--- global_adder_passthrough.jl
+++ global_adder_passthrough.jl
@@ -1,5 +1,6 @@
 using ITensors
-# const NUMBER_REGEX = r"[0-9]+"
+
+###### generalizing to arbitrary leading bits on folded geometry #########
 
 function fraction_to_binary_shift(numerator::Int, denominator::Int, L::Int)
     total_states = 2^L
@@ -50,215 +51,6 @@ function create_addition_tensor_with_carry(shift_bit::Int, s::Index, s_prime::In
     return T
 end
 
-function initialize_links(L, qubit_site, shift_1_3_bits, ram_phy)
-    carry_links = [Index(2, "Carry,c=$(ram_pos-1)") for ram_pos in 1:L+1]
-    T_vec = [ram_pos % 2 == 1 ? create_addition_tensor_with_carry(shift_1_3_bits[ram_phy[ram_pos]], qubit_site[ram_pos], prime(qubit_site[ram_pos]), carry_links[ram_pos+1], carry_links[ram_pos]) : create_addition_tensor_with_carry(shift_1_3_bits[ram_phy[ram_pos]], qubit_site[ram_pos], prime(qubit_site[ram_pos]), carry_links[ram_pos], carry_links[ram_pos+1]) for ram_pos in 1:L];
-    id_vec = [ram_pos % 2 == 1 ? create_identity_tensor_4d(qubit_site[ram_pos], prime(qubit_site[ram_pos]), carry_links[ram_pos], carry_links[ram_pos+1]) : create_identity_tensor_4d(qubit_site[ram_pos], prime(qubit_site[ram_pos]), carry_links[ram_pos], carry_links[ram_pos+1]) for ram_pos in 2:L];
-    gate_vec = [T_vec[ram_pos] * id_vec[ram_pos] for ram_pos in 1:L-1];
-    return carry_links, T_vec, id_vec, gate_vec
-end
-
-function global_adder(initial_state, carry_links, T_vec, gate_vec, qubit_site; cutoff::Float64=0.0, maxdim::Int=2^(div(length(gate_vec),2)))
-    for i1 in 1:length(gate_vec)
-        gate = gate_vec[i1]
-        # println("gate index: ", i1, " ", inds(gate))
-        # println(i1)
-        if i1 == 1
-            # psi_tensor = contract(initial_state[i1], initial_state[i1+1], gate)
-            psi_tensor = gate * (initial_state[i1] * initial_state[i1+1])
-            # println(inds(psi_tensor))
-            # println(Base.summarysize(psi_tensor))
-            U, S, V = svd(psi_tensor, [prime(qubit_site[i1]), carry_links[i1]], lefttags = "Link,l=$(i1)", cutoff=cutoff, maxdim=maxdim)
-            # println(Base.summarysize(U))
-            # println(Base.summarysize(S))
-            # println(Base.summarysize(V))
-            initial_state[i1] = U
-            initial_state[i1+1] = S * V
-            noprime!(initial_state)
-        elseif i1 == 2
-            psi_tensor = gate * (initial_state[i1] * initial_state[i1+1])
-            # println(Base.summarysize(psi_tensor))
-            U, S, V = svd(psi_tensor, [prime(qubit_site[i1]), carry_links[i1], inds(initial_state[i1])[1], inds(initial_state[i1])[2]], lefttags = "Link,l=$(i1)", cutoff=cutoff, maxdim=maxdim)
-            # println(Base.summarysize(U))
-            # println(Base.summarysize(S))
-            # println(Base.summarysize(V))
-            initial_state[i1] = U
-            initial_state[i1+1] = S * V
-            noprime!(initial_state)
-        else
-            # psi_tensor = contract(initial_state[i1-1], initial_state[i1], initial_state[i1+1], gate)
-            psi_tensor = gate * (initial_state[i1-1] * initial_state[i1] * initial_state[i1+1])
-            # println(Base.summarysize(psi_tensor))
-            left_inds_1 = i1 == 3 ? [qubit_site[i1-1], carry_links[i1-1], inds(initial_state[i1-1])[3]] : [inds(initial_state[i1-1])[1], qubit_site[i1-1]]
-            # println(Base.summarysize(left_inds_1))
-            U1, S1, V1 = svd(psi_tensor, left_inds_1, lefttags = "Link,l=$(i1-1)", cutoff=cutoff, maxdim=maxdim)
-            # println(Base.summarysize(U1))
-            # println(Base.summarysize(S1))
-            # println(Base.summarysize(V1))
-            U2, S2, V2 = svd(S1 * V1, [prime(qubit_site[i1]), carry_links[i1+1], inds(U1)[end]], lefttags = "Link,l=$(i1)", cutoff=cutoff, maxdim=maxdim)
-            # println(Base.summarysize(U2))
-            # println(Base.summarysize(S2))
-            # println(Base.summarysize(V2))
-            initial_state[i1-1] = U1
-            initial_state[i1] = U2
-            initial_state[i1+1] = S2 * V2
-            noprime!(initial_state)
-        end
-    end
-    
-    # lsb_tensor = ITensor(carry_links[2])
-    # lsb_tensor[carry_links[2]=>1] = 1.0
-    # initial_state[2] = initial_state[2] * lsb_tensor;
-
-    lsb = onehot(carry_links[2]=>1)
-    initial_state[2] = initial_state[2] * lsb;
-    
-    # discard the carry out of the msb tensor
-    msb_tensor = ITensor(carry_links[1])
-    msb_tensor[carry_links[1]=>1] = 1.0
-    msb_tensor[carry_links[1]=>2] = 1.0
-    initial_state[1] = initial_state[1] * msb_tensor;
-    
-    psi = contract(initial_state[L-1], initial_state[L], T_vec[L])
-    # println(Base.summarysize(psi))
-    # println(setdiff(inds(psi), filterinds(inds(psi), tags="Qubit,Site,n=$L")))
-    # println([inds(initial_state[L-1])[1], qubit_site[L-1]])
-    U, S, V = svd(psi, [inds(initial_state[L-1])[1], qubit_site[L-1]], lefttags = "Link,l=$(L-1)", cutoff=cutoff, maxdim=maxdim)
-    # println(Base.summarysize(U))
-    # println(Base.summarysize(S))
-    # println(Base.summarysize(V))
-    initial_state[L-1] = U
-    initial_state[L] = S * V
-    return initial_state
-end
-
-function entanglement_spectrum(psi::MPS, cut_position::Int)
-    # Make a copy and move orthogonality center
-    psi_copy = copy(psi)
-    orthogonalize!(psi_copy, cut_position)
-    
-    # Get the bond connecting cut_position and cut_position+1
-    bond_idx = commonind(psi_copy[cut_position], psi_copy[cut_position+1])
-    # println(bond_idx)
-    # println(psi_copy[cut_position])
-    # SVD the tensor at cut_position
-    U, S, V = svd(psi_copy[cut_position], bond_idx)
-    
-    return diag(S)
-end
-
-function check_entanglement_spectrum(initial_state::MPS, ram_phy::Vector{Int}, shift_bits::Vector{Int})
-    L = length(shift_bits)
-    for cut in 1:L-1
-        spectrum = entanglement_spectrum(initial_state, cut)
-        println("Cut between sites $cut and $(cut+1): ", spectrum)
-    end
-end
-
-function print_nonzero_coordinates(T)
-    # Get the dimensions of the tensor
-    dims = size(T)
-    
-    # Iterate through all possible indices
-    for idx in CartesianIndices(dims)
-        # Check if element is nonzero
-        if T[idx] != 0
-            # Convert CartesianIndex to tuple for cleaner output
-            coords = Tuple(idx)
-            println("Nonzero at coordinates $coords: $(T[idx])")
-        end
-    end
-end
-
-function sort_indices_by_tag_number(indices)
-    # Extract number from tag string
-    function extract_tag_number(idx)
-        tag_str = string(tags(idx))
-        m = match(r"[a-zA-Z]=(\d+)", tag_str)
-        return m !== nothing ? parse(Int, m.captures[1]) : 0
-    end
-    
-    return sort(collect(indices), by=extract_tag_number)
-end
-
-# function filter_highest_tag_number(indices)
-#     # Extract all tag numbers from indices
-#     tag_numbers = Int[]
-#     println("filter_highest_tag_numberasd")
-#     for idx in indices
-#         tag_str = string(tags(idx))
-#         # Extract numbers from the tag string using regex
-#         numbers = [parse(Int, m.match) for m in eachmatch(r"[0-9]+", tag_str)]
-#         append!(tag_numbers, numbers)
-#     end
-#     # println(tag_numbers)
-#     if isempty(tag_numbers)
-#         return indices
-#     end
-    
-#     max_number = maximum(tag_numbers)
-    
-#     # Filter out indices that contain the maximum number in their tag string
-#     return filter(idx -> !occursin(string(max_number), string(tags(idx))), indices)
-# end
-
-function filter_highest_tag_number(indices)
-    
-    # Pre-allocate with reasonable size hint
-    begin
-        tag_numbers = Vector{Int}()
-        sizehint!(tag_numbers, length(indices) * 2)
-    end
-    
-    # Pre-compute tag strings to avoid repeated string() calls
-    begin
-        tag_strings = Vector{String}(undef, length(indices))
-        for (i, idx) in enumerate(indices)
-            tag_strings[i] = string(tags(idx))
-        end
-    end
-    
-    # Extract numbers from pre-computed strings
-    begin
-        for tag_str in tag_strings
-            for m in eachmatch(NUMBER_REGEX, tag_str)
-                push!(tag_numbers, parse(Int, m.match))
-            end
-        end
-    end
-    
-    begin
-        if isempty(tag_numbers)
-            return indices
-        end
-        
-        max_number = maximum(tag_numbers)
-        max_number_str = string(max_number)  # Convert once
-    end
-    
-    # Use pre-computed tag strings for filtering - return vector always
-    begin
-        # Always return a vector to avoid tuple conversion overhead
-        filtered_indices = eltype(indices)[]
-        for (i, idx) in enumerate(indices)
-            if !occursin(max_number_str, tag_strings[i])
-                push!(filtered_indices, idx)
-            end
-        end
-        return filtered_indices
-    end
-end
-
-function sort_indices(tensor::ITensor, _tags::String)
-    indices = filterinds(inds(tensor), _tags)
-    sorted_inds = sort(collect(indices), by = x -> begin
-        tag_str = string(tags(x))
-        m = match(r"c=(\d+)", tag_str)
-        parse(Int, m.captures[1])
-    end)
-    return sorted_inds
-end
-
 function lower_prime_level(tensor::ITensor, _tags::String)
     # this function lower the double prime level indices to single prime level
     for index in filterinds(inds(tensor), _tags)
@@ -268,47 +60,6 @@ function lower_prime_level(tensor::ITensor, _tags::String)
     end
     return tensor
 end
-
-# set the boundary condition so that we are not carrying dangling indices around
-function build_adder_mpo(qubit_site::Vector{Index{Int64}},L::Int,carry_links::Vector{Index{Int64}},gate_vec::Vector{ITensor}, T_vec::Vector{ITensor},_eps::Float64,_maxdim::Int)
-    lsb_tensor = onehot(carry_links[2]=>1)
-
-    # discard the carry out of the msb tensor
-    msb_tensor = ITensor(carry_links[1])
-    msb_tensor[carry_links[1]=>1] = 1.0
-    msb_tensor[carry_links[1]=>2] = 1.0
-
-    gate_vec[1] = gate_vec[1] * msb_tensor
-    gate_vec[2] = gate_vec[2] * lsb_tensor
-    mpo_vec = [ITensor() for _ in 1:L];
-
-    tmp_tensor = gate_vec[1] * prime(gate_vec[2], "Qubit,Site,n=2") * prime(gate_vec[3], "Qubit,Site,n=3")
-    tmp_tensor = lower_prime_level(tmp_tensor, "Qubit,Site")
-
-    U, S, V = svd(tmp_tensor, [qubit_site[1], prime(qubit_site[1])], lefttags = "Link,l=1", cutoff=_eps, maxdim=_maxdim);
-
-    mpo_vec[1] = U
-    i1 = 4
-    while i1 <= length(gate_vec)
-        tmp_tensor = lower_prime_level(S * V * prime(gate_vec[i1], "Qubit,Site,n=$(i1)"), "Qubit,Site")
-        U, S, V = svd(tmp_tensor, [qubit_site[i1-2], prime(qubit_site[i1-2]), filterinds(tmp_tensor, tags="Link,l=$(i1-3)")...], lefttags = "Link,l=$(i1-2)", cutoff=_eps, maxdim=_maxdim);
-        # println(filter(x -> x >= 1e-10, vec(array(S))))
-        mpo_vec[i1-2] = U
-        i1 += 1
-    end
-
-    U, S, V = svd(S * V, [qubit_site[end-2], prime(qubit_site[end-2]), filterinds(S * V, tags="Link,l=$(L-3)")...], lefttags = "Link,l=$(L-2)", cutoff=_eps, maxdim=_maxdim);
-    mpo_vec[L-2] = U
-    U, S, V = svd(lower_prime_level(S * V * prime(T_vec[L], "Qubit,Site,n=$(L)"), "Qubit,Site"), [qubit_site[L-1], prime(qubit_site[L-1])], lefttags = "Link,l=$(L-1)", cutoff=_eps, maxdim=_maxdim);
-    mpo_vec[L-1] = U
-    mpo_vec[L] = S * V
-
-    return MPO(mpo_vec)
-
-end
-
-
-###### generalizing to arbitrary leading bits on folded geometry #########
 
 function conn_pairs(L::Int)
     # Generate list of right translations of 1:L more efficiently
@@ -496,7 +247,8 @@ function collapse_gate_vec(gate_vec_sorted)
     return gate_vec_collapsed
 end
 
-function create_mpo(gate_vec_collapsed)
+function create_mpo(gate_vec_sorted)
+    gate_vec_collapsed = collapse_gate_vec(gate_vec_sorted)
     mpo_vec = ITensor[]
     tmp = gate_vec_collapsed[1]
     for num_gate in 1:length(gate_vec_collapsed) - 1
@@ -518,26 +270,113 @@ function create_mpo(gate_vec_collapsed)
     return MPO(mpo_vec)
 end
 
-function adder_mpo_vec(L, qubit_site, ancilla, folded, numerator, denominator)
-    adder_mpo_vec = MPO[]
+function adder_mpo_vec_single_number(i1::Int, L, qubit_site, ancilla, folded, numerator, denominator)
     conn_pairs_dict, folded_ram_phy_dict = conn_pairs(L)
-    for i1 in 1:L
-        shift_bits, _ = fraction_to_binary_shift(numerator, denominator, L)
-        # qubit_site, _, _, _ = _initialize_basis(L, ancilla, folded)
-        ram_phy = folded_ram_phy_dict[i1]
-        pairing_fixed_i1 = conn_pairs_dict[i1]
+    shift_bits, _ = fraction_to_binary_shift(numerator, denominator, L)
+    # qubit_site, _, _, _ = _initialize_basis(L, ancilla, folded)
+    ram_phy = folded_ram_phy_dict[i1]
+    pairing_fixed_i1 = conn_pairs_dict[i1]
 
-        # initialize the gate vector
-        gate_vec = initialize_gate_vec(pairing_fixed_i1, shift_bits, qubit_site, L, ram_phy)
+    # initialize the gate vector
+    gate_vec = initialize_gate_vec(pairing_fixed_i1, shift_bits, qubit_site, L, ram_phy)
 
-        # sort the gate vector according to the qubit site number
-        gate_sites = [qubit_site_number(gate) for gate in gate_vec]
-        gate_vec_sorted = gate_vec[sortperm(gate_sites)];
-        gate_vec_collapsed = collapse_gate_vec(gate_vec_sorted);
+    # sort the gate vector according to the qubit site number
+    gate_sites = [qubit_site_number(gate) for gate in gate_vec]
+    gate_vec_sorted = gate_vec[sortperm(gate_sites)];
 
-        # create the mpo
-        adder_mpo = create_mpo(gate_vec_collapsed);
-        push!(adder_mpo_vec, adder_mpo)
+    # create the mpo
+    adder_mpo = create_mpo(gate_vec_sorted);
+    return adder_mpo
+end
+
+function adder_mpo_vec(L::Int64, xj::Set{Rational{Int64}}, qubit_site::Vector{Index{Int64}}, ancilla::Int64, folded::Bool, phy_list::Vector{Int64}, phy_ram::Vector{Int64}, _maxdim::Int64)
+    adder_mpo_vec = MPO[]
+    if xj == Set([1 // 3, 2 // 3])
+        for i1 in 1:L
+            add_1_3 = adder_mpo_vec_single_number(i1, L, qubit_site, ancilla, folded, 1, 3)
+            add_1_6 = adder_mpo_vec_single_number(i1, L, qubit_site, ancilla, folded, 1, 6)
+            # println("bond dims before fix spurs: ", all_bond_dim(add_1_3), " ", all_bond_dim(add_1_6))
+            i2=phy_list[mod(i1,L)+1]    # 2
+            add_condition=apply(add_1_6,P_MPO([phy_ram[i2]],[0],qubit_site)) + apply(add_1_3,P_MPO([phy_ram[i2]],[1],qubit_site))
+            iLm2=phy_list[mod(i1+L-4,L)+1]  # L-2
+            iLm1=phy_list[mod(i1+L-3,L)+1 ]   # L-1
+            iL=phy_list[mod(i1+L-2,L)+1] # L i1+(L-1) -> L, (x-1)%L+1
+            P2=(P_MPO([phy_ram[i1],phy_ram[iLm2],phy_ram[iL]],[1,0,1],qubit_site)+P_MPO([phy_ram[i1],phy_ram[iLm2],phy_ram[iL]],[0,1,0],qubit_site))    # fix spurs
+            XI=XI_MPO([phy_ram[iLm1]],qubit_site)
+
+            fix_spurs = apply(XI,P2) + I_MPO([phy_ram[iLm1]],qubit_site)
+            # println("bond dims after fix spurs: ", all_bond_dim(apply(fix_spurs,add_condition)))
+            push!(adder_mpo_vec, apply(fix_spurs,add_condition))
+        end
+    else
+        error("Not implemented yet")
+        push!(adder_mpo_vec, nothing)
     end
     return adder_mpo_vec
+end
+######### helper functions for testing #########
+
+function all_bond_dim(mp::Union{MPO,MPS})
+    dim_list = []
+    for i in 1:length(mp)-1
+        dim = commonind(mp[i], mp[i+1])
+        push!(dim_list, space(dim))
+    end
+    return dim_list
+end
+
+function entanglement_spectrum(psi::MPS, cut_position::Int)
+    # Make a copy and move orthogonality center
+    psi_copy = copy(psi)
+    orthogonalize!(psi_copy, cut_position)
+    
+    # Get the bond connecting cut_position and cut_position+1
+    bond_idx = commonind(psi_copy[cut_position], psi_copy[cut_position+1])
+    # println(bond_idx)
+    # println(psi_copy[cut_position])
+    # SVD the tensor at cut_position
+    U, S, V = svd(psi_copy[cut_position], bond_idx)
+    
+    return diag(S)
+end
+
+function check_entanglement_spectrum(initial_state::MPS, ram_phy::Vector{Int}, shift_bits::Vector{Int})
+    L = length(shift_bits)
+    for cut in 1:L-1
+        spectrum = entanglement_spectrum(initial_state, cut)
+        println("Cut between sites $cut and $(cut+1): ", spectrum)
+    end
+end
+
+function print_nonzero_coordinates(T)
+    # Get the dimensions of the tensor
+    dims = size(T)
+    
+    # Iterate through all possible indices
+    for idx in CartesianIndices(dims)
+        # Check if element is nonzero
+        if T[idx] != 0
+            # Convert CartesianIndex to tuple for cleaner output
+            coords = Tuple(idx)
+            println("Nonzero at coordinates $coords: $(T[idx])")
+        end
+    end
+end
+
+
+function display_state(state)
+    contracted = contract(state)
+    state_vec = vec(Array(contracted, inds(contracted)...))
+    # @show argmax(state_vec)
+    loc = findall(x->abs(x)>1e-1, state_vec)
+    # println(state_vec[loc])
+    if length(loc) > 0
+        pos = loc[1] - 1
+        # ITensors native ordering: site 1 = rightmost bit in binary position
+        # We reversed input, so reverse output to match our convention
+        binary = string(pos, base=2, pad=L)
+        return reverse(binary)  # site 1 = leftmost bit in our convention
+    else
+        return "0"^L
+    end
 end
